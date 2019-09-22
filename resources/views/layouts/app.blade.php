3e8f099a8464e5e9b407c654ce00e85888dd57c2
Adam Greenfield <ajamesgreenfield@gmail.com>
2019-09-22 10:47:31 +0100

    add auth routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                        </li>
+
+                    @endauth
 
                     </ul>
 
