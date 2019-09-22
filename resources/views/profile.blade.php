81a7d9ee7709ada1d231a9d7f500aa65ec25ceaf
Adam Greenfield <ajamesgreenfield@gmail.com>
2019-09-22 11:19:16 +0100

    added profile view starting calender

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    Hello {{ $user->name }}
+    <p>Upcoming games</p>
+    <p>character sheet</p>
+    <p>gold</p>
+    <p>exp</p>
+</div>
+@stop
