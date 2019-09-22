4f3f8b934bd109cb215464c6f3495d154c2c6f8d
Adam Greenfield <ajamesgreenfield@gmail.com>
2019-09-22 11:36:54 +0100

    refactor calender to game

---
+++ database/migrations/2019_09_22_102643_create_game_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->time('time');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game');
+    }
+}
